82cb44971e6bf31dc55b113945519682368a14e5
dinies <edoardo.ghini@live.it>
2018-09-12 11:38:49 +0200

    added two dummy scripts oipts one to test the plotting interface (now working at last) and the other to try the basic keras tutorial ( which right now does not succed in training)

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # SpikingCNN
 Build a spiking convolutional neural network in tensorflow for image recognitioon tasks
+
+
+
+
+Guide to set up the environment,
+tested on linux 18.04LTS
+
+pyenv and virtualenv set up :
+
+
+sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+
+Add at .bashrc file
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+eval "$(pyenv init -)"
+fi
+
+Source it:
+source $HOME/.bashrc
+
+Virtualenv plug in:
+git clone https://github.com/yyuu/pyenv-virtualenv.git   $HOME/.pyenv/plugins/pyenv-virtualenv
+source $HOME/.bashrc
+
+Remainder of useful pyenv commands:
+pyenv install -l
+pyenv versions
+pyenv virtualenv 3.6.5 venv_name
+pyenv activate venv_name
+pyenv deactivate
+pyenv commands
+
+Our set up: install 3.6.5 and create a virtualenv for tensorflow
+
---
+++ models/dummyNN.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+
+import matplotlib
+matplotlib.use("qt5agg")
+import matplotlib.pyplot as plt
+
+# print(tf.__version__)
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+print( np.argmax( predictions[0]))
