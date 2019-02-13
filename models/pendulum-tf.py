6c8939297a1bb25d7dbddd149e4088da3a97d692
dinies <edoardo.ghini@live.it>
2019-02-13 14:55:35 +0100

    pendulum in tensorflow completed

--- models/pendulum-tf.py
+++ models/pendulum-tf.py
@@ -4,64 +4,81 @@ import numpy as np
 
 
 
-simTime = 10
-deltaT= 0.01
-numSteps = int(simTime/deltaT)
-steps = range(numSteps)
-thetaData =[]
-thetaDotData =[]
+class Pendulum( object):
+    def __init__(self):
+        self.initial_state = [ 0.1 , 0.0 ]
+        self.mass = 4.0
+        self.length = 7.0
+        self.gravity = 9.81
+        self.friction = 0.01
+        self.simTime = 10
+        self.deltaT= 0.01
+        self.numSteps = int(self.simTime/self.deltaT)
+        self.steps = range(self.numSteps)
+        self.thetaData =[]
+        self.thetaDotData =[]
+        self.graph = tf.Graph()
 
-graph = tf.Graph()
-with graph.as_default(): 
+        self.define_graph()
 
-    state = tf.Variable(  [0.01,0.0], name='q')
-    u = tf.placeholder(tf.float32,name='u')
-    dt = tf.placeholder(tf.float32,name='dt')
-    gravity = tf.constant( 9.81, name='g')
-    mass = tf.constant( 4.0,name='m')
-    length = tf.constant( 7.0,name='l')
-    damping = tf.constant(0.00001,name='damping')
-    dq = [
-            state[1],
-            - (gravity / length) * tf.math.sin( state[0]) + u - state[1]*damping
+    def define_graph( self ):
+
+        with self.graph.as_default(): 
+
+            state = tf.Variable( self.initial_state , name='q')
+            self.u = tf.placeholder(tf.float32,name='u')
+            self.dt = tf.placeholder(tf.float32,name='dt')
+            gravity = tf.constant( self.gravity, name='g')
+            mass = tf.constant( self.mass,name='m')
+            length = tf.constant( self.length,name='l')
+            damping = tf.constant(self.friction,name='damping')
+            dq = [
+                state[1],
+                - (gravity / length) * tf.math.sin( state[0]) + self.u - state[1]*damping
             ]
+            dt_vec = [ self.dt , self.dt ]
+            increment_q = tf.multiply( dq, dt_vec, name='dqxdt')
+            update = tf.add( state,increment_q , name='update')
+            self.update_op = tf.assign( state, update, name='save')
+
+            tf.summary.scalar('curr u',self.u)
+            tf.summary.scalar('dq1',dq[0])
+            tf.summary.scalar('dq2',dq[1])
+            tf.summary.scalar('q1',state[0])
+            tf.summary.scalar('q2',state[1])
+            self.summaries = tf.summary.merge_all()
+
+    def run(self):
+
+        with tf.Session(graph= self.graph) as sess:
+
+            writer  = tf.summary.FileWriter('./graphs', sess.graph)
+            sess.run(tf.global_variables_initializer())
+            inputTorque = np.zeros(self.numSteps)
+            inputTorque[1] =0.0
+            k= 0
+            for t in self.steps:
+
+                feed = { self.u: inputTorque[k], self.dt: self.deltaT} 
+                k+=1
+                newState,mergedSummaries= sess.run([self.update_op,self.summaries],feed_dict= feed)
+
+                self.thetaData.append( newState[0] )
+                self.thetaDotData.append( newState[1] )
+                writer.add_summary( mergedSummaries,k)
+        self.plot_data()
 
 
-    tf.summary.scalar('curr u',u)
-    tf.summary.scalar('dq1',dq[0])
-    tf.summary.scalar('dq2',dq[1])
-    tf.summary.scalar('q1',state[0])
-    tf.summary.scalar('q2',state[1])
-    update = tf.assign( state, tf.add( state, dq, name='update'),name='save')
 
-    summaries = tf.summary.merge_all()
+    def plot_data(self):
 
-
-with tf.Session(graph= graph) as sess:
-
-    writer  = tf.summary.FileWriter('./graphs', sess.graph)
-    sess.run(tf.global_variables_initializer())
-
-    inputTorque = np.zeros(numSteps)
-    inputTorque[1] =0.0
-
-    k= 0
-    for t in steps:
+        fig,(ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6.5), tight_layout=True)
+        ax1.plot( self.steps,self.thetaData )
+        ax2.plot( self.steps,self.thetaDotData)
+        plt.show()
         
 
-        feed = { u: inputTorque[k], dt: deltaT} 
-        k+=1
-        newState , mergedSummaries=  sess.run( [update,summaries] , feed_dict= feed)
-
-        thetaData.append( newState[0] )
-        thetaDotData.append( newState[1] )
-        writer.add_summary( mergedSummaries,k)
-
-
-
-fig,(ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6.5), tight_layout=True)
-ax1.plot( steps,thetaData )
-ax2.plot( steps,thetaDotData)
-plt.show()
-        
+if __name__ == '__main__':
+    p= Pendulum()
+    p.run()
  
